bc6087b49a9ef4208217a06f1147b5a1ea9fed21
Jestin Keaton <jkeaton@spsu.edu>
2015-02-23 01:39:46 -0500

    Fixed some spacing

--- dist/dbconnect.php
+++ dist/dbconnect.php
@@ -2,7 +2,7 @@
     function db_connect(){
         $serverName = "(local)";
         $connectionOptions = array("Database"=>"master", "UID"=>"sa", "PWD"=>"Spsu20!4");
-	$link = sqlsrv_connect($serverName, $connectionOptions)
+        $link = sqlsrv_connect($serverName, $connectionOptions)
             or die();
         return $link;
     }
