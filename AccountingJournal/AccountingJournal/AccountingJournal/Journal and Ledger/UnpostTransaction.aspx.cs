2cbbc9780fc03b4748f2513d0fce2351600e7e8c
Jestin Keaton <jkeaton@spsu.edu>
2015-04-10 02:52:44 -0400

    Fixed the bug where debits do not always appear in an entry before all credits.

--- AccountingJournal/AccountingJournal/AccountingJournal/Journal and Ledger/UnpostTransaction.aspx.cs
+++ AccountingJournal/AccountingJournal/AccountingJournal/Journal and Ledger/UnpostTransaction.aspx.cs
@@ -21,7 +21,11 @@ namespace AccountingJournal.Journal_and_Ledger
         private void DisplayUnpostTranx()
         {
             List<IndiJournal> unpost = Connection.DisplayUnpostTranx().Cast<IndiJournal>().ToList();
+            List<String> dr_html = new List<String>();
+            List<String> cr_html = new List<String>();
             StringBuilder sb = new StringBuilder();
+            StringBuilder sb_dr = new StringBuilder();
+            StringBuilder sb_cr = new StringBuilder();
             List<int> id = new List<int>();
             List<JourHeader> Journalheader = new List<JourHeader>();
             List<JourLine> JournalLine = new List<JourLine>();
@@ -49,51 +53,59 @@ namespace AccountingJournal.Journal_and_Ledger
                 }
 
             }
-            for (int i = 0; i < Journalheader.Count; i++)
-            {
-                int reset = 0;
-                for (int j = 0; j < JournalLine.Count; j++)
-                {
-                    if (Journalheader[i].id == JournalLine[j].id)
-                    {
-                        if (reset == 0)
-                        {
-                            reset = 1;
-                            sb.Append(string.Format(@"<tr>"));
-
-                            sb.Append(string.Format(@"<td class='text-center' rowspan='{1}'>{0}</td>", Journalheader[i].Date.ToShortDateString(), Journalheader[i].TotalAccEff + 1));
-                            if (JournalLine[j].IsDebit == "Debit")
-                            {
-                                sb.Append(string.Format(@"<td style='text-align:left'><a class='link' href='../Journal and Ledger/General Ledger.aspx?ID={1}'style='text-decoration:none;'>{0}</a></td>", JournalLine[j].Account, JournalLine[j].AccNum));
-                            }
-                            else
-                            {
-                                sb.Append(string.Format(@"<td style='text-align:left'><a class='link' href='../Journal and Ledger/General Ledger.aspx?ID={1}' style='text-decoration:none; margin-left:20px;'>{0}</a></td>", JournalLine[j].Account, JournalLine[j].AccNum));
-                            }
-                            sb.Append(string.Format(@"<td class='text-right'><a href='../Journal and Ledger/General Ledger.aspx?ID={1}'>{0}</a></td>", JournalLine[j].AccNum, JournalLine[j].AccNum));
-                            sb.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Debit)));
-                            sb.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Credit)));
-                            sb.Append(string.Format(@"<td class='text-center vcenter' rowspan='{1}'><button class='btn btn-primary form-control' value='{0}'>Post</button><br/><br /><button class='btn btn-danger form-control' value'{0}'>Reject</button></td>", Journalheader[i].id, Journalheader[i].TotalAccEff + 1));
-                            sb.Append(string.Format(@"</tr>"));
+            // For each transaction
+            for (int i = 0; i < Journalheader.Count; i++) {
+                // First create each row's string and place that string in the correct list
+                dr_html.Clear();
+                cr_html.Clear();
+                for (int j = 0; j < JournalLine.Count; j++) {
+                    if (Journalheader[i].id == JournalLine[j].id) {
+                        if (JournalLine[j].IsDebit == "Debit"){
+                            sb_dr.Clear();
+                            sb_dr.Append(string.Format(@"<td style='text-align:left'><a class='link' href='../Journal and Ledger/General Ledger.aspx?ID={1}'style='text-decoration:none;'>{0}</a></td>", JournalLine[j].Account, JournalLine[j].AccNum));
+                            sb_dr.Append(string.Format(@"<td class='text-right'><a href='../Journal and Ledger/General Ledger.aspx?ID={1}'>{0}</a></td>", JournalLine[j].AccNum, JournalLine[j].AccNum));
+                            sb_dr.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Debit)));
+                            sb_dr.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Credit)));
+                            dr_html.Add(sb_dr.ToString());
                         }
-                        else
-                        {
-                            sb.Append(string.Format(@"<tr>"));
-                            if (JournalLine[j].IsDebit == "Debit")
-                            {
-                                sb.Append(string.Format(@"<td style='text-align:left'><a href='../Journal and Ledger/General Ledger.aspx?ID={1}'style='text-decoration:none;'>{0}</a></td>", JournalLine[j].Account, JournalLine[j].AccNum));
-                            }
-                            else
-                            {
-                                sb.Append(string.Format(@"<td style='text-align:left'><a href='../Journal and Ledger/General Ledger.aspx?ID={1}' style='text-decoration:none; margin-left:20px;'>{0}</a></td>", JournalLine[j].Account, JournalLine[j].AccNum));
-                            }
-                            sb.Append(string.Format(@"<td class='text-right'><a href='../Journal and Ledger/General Ledger.aspx?ID={1}'>{0}</a></td>", JournalLine[j].AccNum, JournalLine[j].AccNum));
-                            sb.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Debit)));
-                            sb.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Credit)));
-                            sb.Append(string.Format(@"</tr>"));
+                        else {
+                            sb_cr.Clear();
+                            sb_cr.Append(string.Format(@"<td style='text-align:left'><a class='link' href='../Journal and Ledger/General Ledger.aspx?ID={1}' style='text-decoration:none; margin-left:20px;'>{0}</a></td>", JournalLine[j].Account, JournalLine[j].AccNum));
+                            sb_cr.Append(string.Format(@"<td class='text-right'><a href='../Journal and Ledger/General Ledger.aspx?ID={1}'>{0}</a></td>", JournalLine[j].AccNum, JournalLine[j].AccNum));
+                            sb_cr.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Debit)));
+                            sb_cr.Append(string.Format(@"<td class='text-right'>{0}</td>", string.Format("{0:#,##0.00}", JournalLine[j].Credit)));
+                            cr_html.Add(sb_cr.ToString());
                         }
                     }
                 }
+                Boolean firstLine = true; // Tells us whether or not to add the date and Post/Reject buttons
+                // First handle adding the debit rows
+                foreach (String s in dr_html)
+                {
+                    if (firstLine)
+                    {
+                        sb.Append(string.Format(@"<tr>"));
+                        sb.Append(string.Format(@"<td class='text-center' rowspan='{1}'>{0}</td>", Journalheader[i].Date.ToShortDateString(), Journalheader[i].TotalAccEff + 1));
+                        sb.Append(s);
+                        sb.Append(string.Format(@"<td class='text-center vcenter' rowspan='{1}'><button class='btn btn-primary form-control' value='{0}'>Post</button><br/><br /><button class='btn btn-danger form-control' value'{0}'>Reject</button></td>", Journalheader[i].id, Journalheader[i].TotalAccEff + 1));
+                        sb.Append(string.Format(@"</tr>"));
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        sb.Append(string.Format(@"<tr>"));
+                        sb.Append(s);
+                        sb.Append(string.Format(@"</tr>"));
+                    }
+                }
+                // Now handle adding the credit rows
+                foreach (String s in cr_html)
+                {
+                    sb.Append(string.Format(@"<tr>"));
+                    sb.Append(s);
+                    sb.Append(string.Format(@"</tr>"));
+                }
+                // Add the description row below all the debits and credits
                 sb.Append(string.Format(@"<tr>"));
                 sb.Append(string.Format(@"<td>"));
                 for (int j = 0; j < JournalDes.Count; j++)
