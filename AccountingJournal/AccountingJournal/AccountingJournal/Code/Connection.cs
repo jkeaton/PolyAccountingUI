9b6bed1f7f8c25f2c8819c0bc078969ccaa3a609
Jestin Keaton <jkeaton@spsu.edu>
2015-04-29 22:44:24 -0400

    Changing casts from db to c#

--- AccountingJournal/AccountingJournal/AccountingJournal/Code/Connection.cs
+++ AccountingJournal/AccountingJournal/AccountingJournal/Code/Connection.cs
@@ -1183,7 +1183,7 @@ namespace AccountingJournal.Code
                         user_info["LName"] = reader.GetString(3);
                         user_info["UType"] = reader.GetInt32(4).ToString();
                         user_info["Email"] = reader.GetString(5);
-                        user_info["IsLoginDisabled"] = reader.GetInt32(6).ToString();
+                        user_info["IsLoginDisabled"] = reader.GetString(6);
                     }
                 }
                 finally
